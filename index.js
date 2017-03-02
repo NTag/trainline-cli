4124709d1a73ae71af95b00cfee8a92d26470854
Basile Bruneau <basilebruneau@gmail.com>
2017-03-02 21:59:22 +0100

    Add the list of user's trips through --trips

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const program = require('commander');
 const prompt = require('prompt');
 const storage = require('node-persist');
 const colors = require('colors');
+const moment = require('moment');
+const Table = require('cli-table2');
 
 // Connected user infos
 var uinfos;
@@ -35,6 +37,7 @@ program
   .version('1.0.0')
   .option('-l, --login [email]', 'Log in to your Trainline account')
   .option('-L, --logout', 'Logout of your Trainline account')
+  .option('-t, --trips', 'List of your trips')
   .parse(process.argv);
 
 function main() {
@@ -50,6 +53,7 @@ function main() {
       }
     }, function (err, result) {
       if (err || !result || !result.password) {
+        console.log('Please enter your password');
         return;
       }
       trainline.connexion(program.login, result.password).then(infos => {
@@ -61,12 +65,49 @@ function main() {
         console.log(colors.red('Wrong password or wrong email address'));
       });
     });
+    return;
+  }
+
+  // The following actions need a user to be connected
+  if (!uinfos) {
+    console.log('You are not connected. Use --login [email]');
+    return;
   }
 
   // Logout
   if (program.logout) {
     storage.removeItem('uinfos').then(() => {
       console.log('You are now disconnected');
+    });
+  }
+
+  if (program.trips) {
+    console.log('List of your trips');
+    trainline.trips().then(trips => {
+      let table = new Table({
+        style: { 'padding-left': 0 }
+      });
+
+      trips.slice(0, 7).forEach(trip => {
+        let reference = trip.reference;
+
+        let departure_date = moment(trip.departure_date).calendar();
+        let arrival_date = moment(trip.arrival_date).calendar();
+        let date = departure_date;
+        if (departure_date != arrival_date) {
+          date += '\n' + arrival_date;
+        }
+
+        let stations = trip.departure_station.name + '\n' + trip.arrival_station.name;
+
+        let passenger = trip.passenger.first_name;
+
+        let price = {hAlign: 'right', content: trip.cents/100 + ' ' + trip.currency};
+
+        table.push([reference, date, stations, passenger, price]);
+      });
+
+      console.log(table.toString());
     });
   }
 }
--- trainline.js
+++ trainline.js
@@ -5,20 +5,40 @@ let trainline = {
   TOKEN: null
 };
 
+/**
+ * Perform a request to the API
+ * @param url string The URL of the resource
+ * @param method string 'GET' or 'POST'
+ * @param object The potential JSON body
+ * @return Promise()
+ */
 function apiRequest(url, method, body) {
   let options = {
     method: method,
     uri: API + url,
-    // qs: {
-    //     access_token: 'xxxxx xxxxx' // -> uri + '?access_token=xxxxx%20xxxxx'
-    // },
-    // headers: {
-    //     'User-Agent': 'Request-Promise'
-    // },
     body: body,
     json: true // Automatically parses the JSON string in the response
   };
+  if (trainline.TOKEN) {
+    options.headers = {
+      Authorization: 'Token token="' + trainline.TOKEN + '"'
+    };
+  }
   return request(options);
+}
+
+/**
+ * Convert and array containing objects with an `id` field
+ * to an object where the keys are the ids
+ * @param arr Array
+ * @return object
+ */
+function arrToObj(arr) {
+  let o = {};
+  arr.forEach(a => {
+    o[a.id] = a;
+  });
+  return o;
 }
 
 /**
@@ -33,6 +53,33 @@ trainline.connexion = function (email, password) {
     password: password
   }).then(infos => {
     return infos;
+  });
+};
+
+/**
+ * List of the user's trips
+ * @return Promise([{arrival_date, departure_date, arrival_station, departure_station, cents}])
+ */
+trainline.trips = function() {
+  return apiRequest('pnrs', 'GET').then(infos => {
+    let stations = arrToObj(infos.stations);
+    let passengers = arrToObj(infos.passengers);
+    let folders = arrToObj(infos.folders);
+    let pnrs = arrToObj(infos.pnrs);
+
+    infos.trips.forEach(trip => {
+      trip.arrival_station = stations[trip.arrival_station_id];
+      trip.departure_station = stations[trip.departure_station_id];
+      trip.passenger = passengers[trip.passenger_id];
+      trip.reference = pnrs[folders[trip.folder_id].pnr_id].code;
+    });
+
+    // Only the booked trips
+    let trips = infos.trips.filter(trip => {
+      return pnrs[folders[trip.folder_id].pnr_id].booking_status == 'emitted';
+    });
+
+    return trips;
   });
 };
 
